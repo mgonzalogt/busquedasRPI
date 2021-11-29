4a04a06b496e217ae7cdec127c4a1479463451f6
rpidevgt <91293685+rpidevgt@users.noreply.github.com>
2021-11-29 12:36:00 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/MarcaDataController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusquedasRPI.Models;
+using DevExtreme.AspNet.Data;
+using DevExtreme.AspNet.Mvc;
+
+namespace BusquedasRPI.Controllers
+{ 
+
+    [Route("api/[controller]")]
+    public class MarcaDataController : Controller
+    {
+
+        [HttpGet]
+        public object Get(DataSourceLoadOptions loadOptions)
+        {
+            return DataSourceLoader.Load(MarcaData.ListaMarcas, loadOptions);
+        }
+
+    }
+}
---
+++ Models/Marca.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusquedasRPI.Models
+{
+    public class Marca
+    {
+        public string Id { get; set; }
+        public string ExpedienteId { get; set; }
+        public string TipoDeMarca { get; set; }
+        public string Denominacion { get; set; }
+    }
+}
