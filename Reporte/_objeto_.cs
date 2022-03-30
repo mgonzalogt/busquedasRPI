7b5766948aee07675571f4011d4cd0b1cdc6e843
24 Horas <24horas@guatebox.com>
2022-03-30 08:54:50 -0600

    Ejemplo Reporte - K Herrera

---
+++ Reporte/_objeto_.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusquedasRPI.Reporte
+{
+    public class _objeto_
+    {
+        public int id { get; set; }
+        public string nada { get; set; }
+    }
+}
---
+++ Reporte/_reporte_.cs
@@ -0,0 +1,13 @@
+using System;
+using DevExpress.XtraReports.UI;
+
+namespace BusquedasRPI.Reporte
+{
+    public partial class _reporte_
+    {
+        public _reporte_()
+        {
+            InitializeComponent();
+        }
+    }
+}
